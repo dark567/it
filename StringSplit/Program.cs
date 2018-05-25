ea5c2bfab7125194d76121b92f4f0712086c6f69
sergey <sergey@ECOTEST-ADMIN>
2018-05-25 15:53:28 +0300

    Добавьте файлы проекта.

---
+++ StringSplit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t1 = "HumaClot Quattro; H2870040; 25.05.2018; 13:00:58; 222; Thrombin T.; 1; 111,2;s; void;void; void;void; void;void; void;void; OK; D7B1;";
+
+            Char delimiter = ';';
+            String[] substrings = t1.Split(delimiter);
+
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                Console.WriteLine($"{i} - {substrings[i]}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StringSplit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("StringSplit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringSplit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d7b5bb12-7985-4987-81d9-f1b0013cecdb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
